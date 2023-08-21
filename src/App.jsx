127ff02214a87740f47c7f04ed66b6e5ce1d0bf7
aguiarHugo <aguiarhugofx@gmail.com>
2023-08-21 11:22:10 -0300

    Paddings and backgrounds

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,13 @@ const App = () => {
       <section className='padding-x py-10'>
         Services
       </section>
-      <section className='padding'>
+      <section className='bg-pale-blue padding'>
         CustomerReviews
       </section>
-      <section className='padding'>
+      <section className='padding-x sm:py-32 py-16 w-full'>
         Subscribe
       </section>
-      <section className='padding'>
+      <section className='bg-black padding-x padding-t pb-8'>
         Footer
       </section>
     </main>
