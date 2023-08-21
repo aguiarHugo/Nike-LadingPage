3817697d2f40941b48e4643a0e163da3d8baa1b4
aguiarHugo <aguiarhugofx@gmail.com>
2023-08-21 11:20:08 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='relative'>
+      Nav
+      <section className='xl:padding-1 wide:padding-r padding-b'>
+        Hero
+      </section>
+      <section className='padding'>
+        PopularProducts
+      </section>
+      <section className='padding'>
+        SuperQuality
+      </section>
+      <section className='padding-x py-10'>
+        Services
+      </section>
+      <section className='padding'>
+        CustomerReviews
+      </section>
+      <section className='padding'>
+        Subscribe
+      </section>
+      <section className='padding'>
+        Footer
+      </section>
+    </main>
+  )
+}
+
+export default App
