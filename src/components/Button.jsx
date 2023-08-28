a1d8b7dcb95b38cda6bf4c00c5f3992b4cbc709a
aguiarHugo <aguiarhugofx@gmail.com>
2023-08-28 10:32:54 -0300

    Added classes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
-const Button = ({ label, iconURL, backgroundColor, textColor, borderColor}) => {
+const Button = ({ label, iconURL, backgroundColor, textColor, borderColor, fullWidth}) => {
   return (
     <button 
-    className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+    className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full ${fullWidth && 'w-full'}
       ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` 
       : 'bg-coral-red  text-white border-coral-red'}`}>
         {label}
